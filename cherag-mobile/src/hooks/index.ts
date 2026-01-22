277c5df720e26eaf0eddca10706717307b06f162
Qamber_Baloch <qamberhanif11@gmail.com>
2026-01-22 19:59:21 +0500

    fixes

--- cherag-mobile/src/hooks/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Hooks Index
- * Export all hooks for easy imports
- */
-
-export { useAuth } from './useAuth';
-export { useFiles } from './useFiles';
-export { useChat } from './useChat';
-export { useFlashcards } from './useFlashcards';
